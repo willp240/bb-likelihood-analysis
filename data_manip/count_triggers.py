f79f69790ed6d681793a118fb343c1fe6f5daf8a
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2017-10-19 15:43:07 +0100

    update count-triggers and save result for b8 events

--- data_manip/count_triggers.py
+++ data_manip/count_triggers.py
@@ -1,26 +1,29 @@
 import ROOT
 
-def count_ev_indices(br_name, *filenames):
-    ch = ROOT.TChain(br_name)
-    ch.Add(filenames)
+def count_ev_indices(t_name, *filenames):
+    ch = ROOT.TChain(t_name)
+    for fn in filenames:
+        ch.Add(fn)
     
     triggers = [0] * 10
     for x in ch:
         triggers[x.evIndex + 1] += 1
-    return triggers
+    return triggers, ch.GetEntries()
     
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_arguments("infile_names", type=str, nargs="+")
-    parser.add_arguments("--tree_name", type=str, default = "output")
+    parser.add_argument("infile_names", type=str, nargs="+")
+    parser.add_argument("--tree_name", type=str, default = "output")
     args = parser.parse_args()
 
-    event_indices = count_ev_indices(args.tree_name, args.infile_names)
+    event_indices, entries = count_ev_indices(args.tree_name, *args.infile_names)
     for i, count in enumerate(event_indices):
         print i-1, count
 
-    sum_ = 0
-    for i, count in enumerate(event_indices):
-        sum_ += count
-        print i, sum_
+print "Number of non-retriggers = ", event_indices[0] + event_indices[1]
+print "Number of entries = ", entries
+
+if entries:
+    print "Correction = ", (event_indices[0] + event_indices[1])*1./entries
+
