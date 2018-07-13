998c7965ade1b75b48621e0384489f4c30636a3e
Jack <jackdunger@gmail.com>
2018-07-13 13:24:49 +0100

    fix count-nuclei script

--- physint/count_nuclei.py
+++ physint/count_nuclei.py
@@ -2,10 +2,10 @@ from rate_conversions import count_nuclei, hl_to_mass
 import math
 
 ELECTRON_MASS = 511e3 # electron volts
-TE_130_MASS   = 129.906 * 1.67e-27 # ~130u
-TE_130_ABUNDANCE  = 0.345
+TE_130_MASS   = 127.60 * 1.67e-27 # ~130u
+TE_130_ABUNDANCE  = 0.3408
 
 SNOT_MASS = 782e3
 SNOT_LOADING = 0.005
 
-print count_nuclei(SNOT_MASS, SNOT_LOADING, TE_130_ABUNDANCE, TE_130_MASS) * math.log(2)/1.5e25
+print count_nuclei(SNOT_MASS, SNOT_LOADING, TE_130_ABUNDANCE, TE_130_MASS)
